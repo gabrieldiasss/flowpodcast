cc8d8360ff6d874dfffa4b6129e41f1a55732768
Gabriel  Dias <gabrieldiascorrea7@gmail.com>
2022-03-10 20:05:23 -0300

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-const { resolve } = require('path')
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  }
+  plugins: [react()]
 })
