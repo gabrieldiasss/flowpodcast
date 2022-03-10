a0ec23ab8ffe9a60eedbb7af2e3c389f8834f442
Gabriel  Dias <gabrieldiascorrea7@gmail.com>
2022-03-10 19:29:35 -0300

    remove nested

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 const { resolve } = require('path')
 import react from '@vitejs/plugin-react'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/index.html')
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
